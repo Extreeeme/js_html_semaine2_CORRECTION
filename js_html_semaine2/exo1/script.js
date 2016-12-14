c2d1d93365a67fdb75f63f1ada4c533bedfca96e
Extreeeme <martindcontact@gmail.com>
2016-12-14 23:26:47 +0100

    exo n°01.2 fini

--- js_html_semaine2/exo1/script.js
+++ js_html_semaine2/exo1/script.js
@@ -1,2 +1,8 @@
+/*Exercice 01.1
 var laVariable = 42;
-alert(laVariable);
+alert(laVariable); */
+
+var prenom = "Martin";
+var nom = "Dagostino";
+var ville = "Moulins";
+alert("Prenom : "+ prenom + "\nNom : " + nom + "\nVille : " + ville);
